b5920cb804b4908e6d28e343cb7631967f47889e
njustfengkj <njustfengkj@hotmail.com>
2020-10-10 21:22:06 +0800

    创建工程

---
+++ src/main/java/com/feng/coincidentally/CoincidentallyApplication.java
@@ -0,0 +1,13 @@
+package com.feng.coincidentally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoincidentallyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoincidentallyApplication.class, args);
+    }
+
+}
